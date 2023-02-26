481dfb79a08ea0f8a0aeef3cf02dedd51015103f
sednaoui <sed@protonmail.com>
2023-02-26 17:29:06 +0100

    remove build folder from repo

--- build/config/network.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NetworkChainIds = exports.ValidNetworks = void 0;
-exports.ValidNetworks = ["Goerli"];
-exports.NetworkChainIds = {
-    Goerli: 5,
-};
