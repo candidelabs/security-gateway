2e97394e4ded2c425eb9f3f3b52a183bd389fcfb
Andrew Wahid <andrew.wahid1@gmail.com>
2024-10-02 23:56:10 +0300

    Deprecate goerli and op-goerli and replace with sepolia and op-sepolia

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,8 +6,8 @@ interface AppEnvironment {
   NODE_ENV: "production" | "development";
   NAME: string;
   PORT: number;
-  GOERLI_RPC: string;
-  OPTIMISM_GOERLI_RPC: string;
+  SEPOLIA_RPC: string;
+  OPTIMISM_SEPOLIA_RPC: string;
   OPTIMISM_RPC: string;
   FINALIZER_SK: string;
   MONGO_URL: string;
@@ -20,8 +20,8 @@ export const Env: AppEnvironment = {
   NAME: "Security Gateway",
   PORT: Number(process.env.CANDIDE_SECURITY_PORT),
   MONGO_URL: process.env.CANDIDE_SECURITY_MONGODB_URL ?? "",
-  GOERLI_RPC: process.env.CANDIDE_SECURITY_GOERLI_RPC ?? "",
-  OPTIMISM_GOERLI_RPC: process.env.CANDIDE_SECURITY_OPTIMISM_GOERLI_RPC ?? "",
+  SEPOLIA_RPC: process.env.CANDIDE_SECURITY_SEPOLIA_RPC ?? "",
+  OPTIMISM_SEPOLIA_RPC: process.env.CANDIDE_SECURITY_OPTIMISM_SEPOLIA_RPC ?? "",
   OPTIMISM_RPC: process.env.CANDIDE_SECURITY_OPTIMISM_RPC ?? "",
   FINALIZER_SK: process.env.CANDIDE_SECURITY_FINALIZER_SK ?? "",
   SENTRY_DSN: process.env.CANDIDE_SECURITY_SENTRY_DSN ?? "",
--- src/utils/rpc.ts
+++ src/utils/rpc.ts
@@ -2,10 +2,10 @@ import { Env, Networks } from "../config";
 
 export const getRPC = (network: Networks) => {
   switch (network) {
-    case "Goerli":
-      return Env.GOERLI_RPC;
-    case "Optimism Goerli":
-      return Env.OPTIMISM_GOERLI_RPC;
+    case "Sepolia":
+      return Env.SEPOLIA_RPC;
+    case "Optimism Sepolia":
+      return Env.OPTIMISM_SEPOLIA_RPC;
     case "Optimism":
       return Env.OPTIMISM_RPC;
 
